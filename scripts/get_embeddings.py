671c67f4b856bd37cfb3f71efa083686cde429f4
Dakoro <david.korobetski@gmail.com>
2024-02-06 15:08:02 +0100

    feat: compute embeddings for 10 000 documents

--- scripts/get_embeddings.py
+++ scripts/get_embeddings.py
@@ -13,10 +13,13 @@ COLS = ['paper_id',
 
 def main():
     df = pd.read_csv("/home/dakoro/Projet_Simplon/sample.csv", names=COLS)
-    model = SentenceTransformer(model_name_or_path='BAAI/bge-large-en-v1.5')
-    df['embeddings'] = df['abstract'].map(lambda s: model.encode(s))
-    df.to_pickle("../sample_with_embeddings.pkl",
-                 protocol=pickle.HIGHEST_PROTOCOL)
+    df = df.iloc[1:]
+    model = SentenceTransformer('BAAI/bge-large-en-v1.5')
+    docs = df['abstract'].to_list()
+    embeddings = model.encode(docs, show_progress_bar=True)
+
+    with open("./embeddings.pkl", 'wb') as pklf:
+        pickle.dump(embeddings, pklf)
 
 
 if __name__ == "__main__":
