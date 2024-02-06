60333c393e33f195fd68658caba48ea479851445
Dakoro <david.korobetski@gmail.com>
2024-02-06 15:07:02 +0100

    refactor: update api for data

--- api/data/main.py
+++ api/data/main.py
@@ -3,12 +3,24 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models import Paper, Author
 
-app = FastAPI()
+app = FastAPI(debug=True)
+
+
+@app.get("/api/papers/{skip}:{limit}")
+async def get_papers_batch(skip, limit, db: Session = Depends(get_db)):
+    data = db.query(Paper).offset(skip).limit(limit).all()
+    return data
 
 
 @app.get("/api/papers/{paper_id}")
 async def get_paper(paper_id, db: Session = Depends(get_db)):
     data = db.query(Paper).filter(Paper.id == paper_id).first()
+    return data
+
+
+@app.get("/api/authors/{skip}:{limit}")
+async def get_authors_batch(skip, limit, db: Session = Depends(get_db)):
+    data = db.query(Author).offset(skip).limit(limit).all()
     return data
 
 
