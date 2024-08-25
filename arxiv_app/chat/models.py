c122b120a1b2d1c561bd048966e2efce0a80ccfa
Dakoro <david.korobetski@gmail.com>
2024-08-25 16:39:57 +0200

    feat: add like and dislike button and add feedback field

--- arxiv_app/chat/models.py
+++ arxiv_app/chat/models.py
@@ -5,4 +5,5 @@ class Message(models.Model):
     user_message = models.TextField()
     bot_message = models.TextField()
     rag_score = models.FloatField(default=-1.0)
+    feedback = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
