9b76cea0e2183bc528e19153251d9bfe3eb49529
Dakoro <david.korobetski@gmail.com>
2024-08-27 14:42:15 +0200

    refactor: revome useless print

--- scripts/aggregation/get_embeddings.py
+++ scripts/aggregation/get_embeddings.py
@@ -8,7 +8,6 @@ EMBEDDINGS_PATH = os.path.join(os.getcwd(), 'files', 'pkl', 'embeddings.pkl')
 
 def main():
     df = pd.read_pickle(SAMPLE_PATH)
-    print(df.columns)
     model = SentenceTransformer('BAAI/bge-large-en-v1.5')
     docs = df['abstract'].to_list()
     embeddings = model.encode(docs, show_progress_bar=True)
