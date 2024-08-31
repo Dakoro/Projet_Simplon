267d8c6cb6b56306d8ba9225b7a07811871d8850
Dakoro <david.korobetski@gmail.com>
2024-08-31 09:05:46 +0200

    refactor: add table args

--- api/model/models.py
+++ api/model/models.py
@@ -4,6 +4,7 @@ from database import Base
 
 class User(Base):
     __tablename__ = "UserApiModel"
+    __table_args__ = {'extend_existing': True}
 
     id = Column(String(255), primary_key=True)
     username = Column(String(255), nullable=False)
